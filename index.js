189645183756cf2142efafb4066f2558e6c85b87
florence <loracodes3@gmail.com>
2024-10-01 18:31:00 +0300

    negate truthy value using ! operator

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 //negate an expression using !
 const truthyValue = 'This value is truthy.';
-const falseyValue = 0;
+const falseyValue = 1;
 
 !truthyValue;
 console.log(!truthyValue)
+
+//The logical NOT operator (!), also called the bang operator, operates on an expression, returning the opposite of the expression's truthiness. If x resolves to a truthy value, !x returns false. If x is falsey, !x returns true.
